4be44707c8b6e429395871f5ec6e8863ed55eca5
tcocagne <tcocagne@cleversafe.com>
2012-07-03 07:52:39 -0500

    Renamed project to txdbus

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 VERSION     = '1.0.0'
-DESCRIPTION = 'Twisted DBus'
+DESCRIPTION = 'Tx DBus'
 
 try:
     import twisted
@@ -12,28 +12,27 @@ from distutils.core import setup
 
 
 setup(
-    name             = 'twisted_dbus',
+    name             = 'txdbus',
     version          = VERSION,
     description      = DESCRIPTION,
     license          = "MIT",
     long_description ="""\
 A native Python implementation of the DBus protocol for the Twisted
 networking framework""",
-    url              = 'https://github.com/cocagne/twisted_dbus',
+    url              = 'https://github.com/cocagne/txdbus',
     author           = "Tom Cocagne",
     author_email     = 'tom.cocagne@gmail.com',
     packages         = ['tx',
                         'tx.dbus',
                         'tx.dbus.test'],
     keywords         = ['dbus', 'twisted'],
-    classifiers      = ['Programming Language :: Python',
-                        'Programming Language :: Python :: 2',
-                        'Development Status :: 4 - Beta',
-                        'Environment :: Other Environment',
+    classifiers      = ['Development Status :: 4 - Beta',
+                        'Framework :: Twisted',
                         'Intended Audience :: Developers',
                         'License :: OSI Approved :: MIT License',
-                        'Operating System :: OS Independent',
-                        'Topic :: Software Development :: Libraries :: Python Modules',
-                        ],
+                        'Operating System :: POSIX',
+                        'Programming Language :: Python',
+                        'Topic :: Software Development :: Libraries',
+                        'Topic :: System :: Networking'],
     )
 
