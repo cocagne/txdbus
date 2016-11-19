462b9d0e2533c986d1571d7846ffa4dcc959f620
exvito <ex.vitorino@gmail.com>
2016-11-19 15:17:14 +0000

    Updated sample UNIX FD server/client highlighting limitations

--- doc/examples/fd_client.py
+++ doc/examples/fd_client.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 
+"""
+fd_client.py
+
+Grabs '/path/to/FDObject' on 'org.example' bus and demonstrates calling
+remote methods with UNIX open file descriptors as arguments (type 'h' as
+per the dbus spec).
+
+NOTE:
+Passing open UNIX filedescriptors accross RPC / ICP mechanisms such as dbus
+requires the underlying transport to be a UNIX domain socket.
+"""
 
 from __future__ import print_function
 
@@ -9,18 +20,54 @@ from txdbus import client
 
 
 @defer.inlineCallbacks
+def call_remote_verbose(obj, method, *args, **kwargs):
+
+    print('%s%s' % (method, args), end=' = ')
+    result = yield obj.callRemote(method, *args, **kwargs)
+    print(repr(result))
+    defer.returnValue(result)
+
+
+@defer.inlineCallbacks
 def main(reactor):
 
+    PATH = '/path/to/FDObject'
+    BUSN = 'org.example'
+
     bus = yield client.connect(reactor)
-    print('connected')
-    object = yield bus.getRemoteObject('org.example', '/path/to/FDObject')
+    print('connected to dbus')
+    object = yield bus.getRemoteObject(BUSN, PATH)
     print('obtained remote object')
 
-    with open(__file__, 'rt') as f:
-        fd = f.fileno()
-        print('calling lenFD(%i) -' % (fd,), end=' ')
-        result = yield object.callRemote('lenFD', f.fileno())
-        print('result=%r' % (result,))
+    # Open this source file. Ask remote to read it and return byte count.
+    with open(__file__, 'rb') as f:
+        yield call_remote_verbose(object, 'lenFD', f.fileno())
+
+    # IMPORTANT
+    # ---------
+    # The following calls are commented out for two reasons:
+    # - Current implementation seems to fail calling a remote method with
+    #   UNIX FDs as arguments more than once. In other words, calling the
+    #   above coded 'lenFD' twice, for example, fails on the second attempt.
+    #   DBus seems to be closing the connection when getting the 2nd call.
+    #   (so, if you want to test the next example call of 'readBytesFD',
+    #   for now you will need to comment out the previous call of 'lenFD')
+    # - Additionally, the current implementation fails at handling calls
+    #   with more than one UNIX FD arument. Failure behavior is different,
+    #   though: the client call goes through, but the server side does not
+    #   seem to handle receiving more than one FD.
+
+#    # Open this source file. Ask remote to read 10 bytes from it.
+#    with open(__file__, 'rb') as f:
+#        yield call_remote_verbose(object, 'readBytesFD', f.fileno(), 10)
+
+#    # Like the previous one - exercise passing two open UNIX FDs.
+#    with open(__file__, 'rb') as f1, open(__file__, 'rb') as f2:
+#        fd1 = f1.fileno()
+#        fd2 = f2.fileno()
+#        yield call_remote_verbose(object, 'readBytesTwoFDs', fd1, fd2, 5)
+
+    bus.disconnect()
 
 
 if __name__ == '__main__':
--- doc/examples/fd_server.py
+++ doc/examples/fd_server.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+"""
+fd_server.py
+
+Publishes FDObject at PATH on BUSN (see code), demonstrating server side
+implementation of methods with open UNIX file descriptors as arguments
+(type 'h' as per the dbus spec).
+
+NOTE::
+Passing open UNIX filedescriptors accross RPC / ICP mechanisms such as dbus
+requires the underlying transport to be a UNIX domain socket.
+"""
+
 
 from __future__ import print_function
 
@@ -10,24 +22,61 @@ from twisted.internet import reactor, defer
 from txdbus import client, objects, interface
 
 
+def trace_method_call(method):
+
+    def wrapper(*args, **kwargs):
+        print('%s%s' % (method.__name__, args[1:]), end=' = ')
+        result = method(*args, **kwargs)
+        print(repr(result))
+        return result
+
+    return wrapper
+
+
 class FDObject(objects.DBusObject):
 
     iface = interface.DBusInterface(
         'org.example.FDInterface',
         interface.Method('lenFD', arguments='h', returns='t'),
+        interface.Method('readBytesFD', arguments='ht', returns='ay'),
+        interface.Method('readBytesTwoFDs', arguments='hht', returns='ay'),
     )
 
     dbusInterfaces = [iface]
 
+    @trace_method_call
     def dbus_lenFD(self, fd):
-        print('handling lenFD(fd=%r)' % (fd,), end=' ')
+        """
+        Returns the byte count after reading till EOF.
+        """
         f = os.fdopen(fd)
         result = len(f.read())
         f.close()
-        print('result=%r' % (result,))
         return result
-        
-    
+
+    @trace_method_call
+    def dbus_readBytesFD(self, fd, byte_count):
+        """
+        Reads byte_count bytes from fd and returns them.
+        """
+        f = os.fdopen(fd)
+        result = f.read(byte_count)
+        f.close()
+        return bytearray(result)
+
+    @trace_method_call
+    def dbus_readBytesTwoFDs(self, fd1, fd2, byte_count):
+        """
+        Reads byte_count from fd1 and fd2. Returns concatenation.
+        """
+        result = bytearray()
+        for fd in (fd1, fd2):
+            f = os.fdopen(fd)
+            result.extend(f.read(byte_count))
+            f.close()
+        return result
+
+
 @defer.inlineCallbacks
 def main(reactor):
 
@@ -35,11 +84,11 @@ def main(reactor):
     BUSN = 'org.example'
 
     bus = yield client.connect(reactor)
-    print('connected')
+    print('connected to dbus')
     object = FDObject(PATH)
     bus.exportObject(object)
     yield bus.requestBusName(BUSN)
-    print('exported object on %r at %r' % (BUSN, PATH))
+    print('exported %r on %r at %r' % (object.__class__.__name__, BUSN, PATH))
 
 
 if __name__ == '__main__':
