a72f2c5c438d6078ddb2470b26ac8c3dd5ad48ea
exvito <ex.vitorino@gmail.com>
2017-07-07 19:19:16 +0100

    Removed no-longer applicable comment.

--- txdbus/router.py
+++ txdbus/router.py
@@ -45,9 +45,6 @@ class Rule (object):
                     return
 
             if hasattr(self, 'path_namespace'):
-                # See https://github.com/cocagne/txdbus/issues/62
-                # FIXME: Skipping validation for this since we already have an
-                # open issue for it:
                 if (m.path is None or
                         not m.path.startswith(self.path_namespace)):
                     return
