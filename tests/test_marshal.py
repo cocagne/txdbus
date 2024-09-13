62d86b932f05d6ea39fc1dba94598dd947a2b6c4
Hugh Daschbach <hdasch@ccss.com>
2024-09-12 23:06:26 -0700

    Fix marshaling tests on big endian systems.
    Fix about 16 marshaling error similar to:
    
    ===============================================================================
    [FAIL]
    Traceback (most recent call last):
      File "/usr/lib/python3.11/unittest/case.py", line 57, in testPartExecutor
        yield
      File "/usr/lib/python3.11/unittest/case.py", line 623, in run
        self._callTestMethod(testMethod)
      File "/usr/lib/python3.11/unittest/case.py", line 579, in _callTestMethod
        if method() is not None:
      File "/home/hugh/txdbus/tests/test_marshal.py", line 240, in test_byte
        self.check('ay', [[1, 2, 3, 4]], pack('iBBBB', 4, 1, 2, 3, 4))
      File "/home/hugh/txdbus/tests/test_marshal.py", line 143, in check
        self.assertEqual(
      File "/usr/lib/python3.11/unittest/case.py", line 873, in assertEqual
        assertion_func(first, second, msg=msg)
      File "/usr/lib/python3.11/unittest/case.py", line 866, in _baseAssertEqual
        raise self.failureException(msg)
    builtins.AssertionError: b'\x04\x00\x00\x00\x01\x02\x03\x04' != b'\x00\x00\x00\x04\x01\x02\x03\x04' : Binary encoding differs from expected value
    
    tests.test_marshal.TestArrayMarshal.test_byte
    ===============================================================================

--- tests/test_marshal.py
+++ tests/test_marshal.py
@@ -1,3 +1,4 @@
+import sys
 import unittest
 from struct import pack
 
@@ -125,7 +126,7 @@ class SignatureIteratorTests(unittest.TestCase):
 
 class TestMarshal(unittest.TestCase):
 
-    def check(self, sig, var_list, expected_encoding, little_endian=True):
+    def check(self, sig, var_list, expected_encoding, little_endian = sys.byteorder == 'little'):
         if not isinstance(var_list, list):
             var_list = [var_list]
         nbytes, chunks = m.marshal(sig, var_list, 0, little_endian)
